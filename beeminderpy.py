754c0caf5d7a4ff2ea2db6af697bfb8c8519d50f
J <stbarnes@stanford.edu>
2021-02-14 07:56:46 -0600

    Add update_datapoint and set_day_datapoint

--- beeminderpy.py
+++ beeminderpy.py
@@ -1,5 +1,7 @@
 from urllib.parse import urlencode
 from urllib.request import urlopen, Request
+import json
+import datetime
 # API details at https://www.beeminder.com/api
 
 
@@ -21,17 +23,19 @@ class Beeminder:
     result = self.call_api(url, params, 'GET')
     return result
 
-  def get_datapoints(self, username, goalname, page=None, per=None, count=None):
+  def get_datapoints(self, username, goalname, sort=None, page=None, per=None, count=None):
     url = "%s/users/%s/goals/%s/datapoints.json" % (self.base_url, username, goalname)
     params = {
         'auth_token': self.auth_token,
         'page': page,
         'per': per,
         'count': count,
+        'sort': sort,
         }
 
     result = self.call_api(url, params, 'GET')
-    return result
+    datapoints = json.loads(result)
+    return datapoints
 
   def create_datapoint(self, username, goalname, value, timestamp=None, daystamp=None, comment=None, requestid=None):
     url = "%s/users/%s/goals/%s/datapoints.json" % (self.base_url, username, goalname)
@@ -47,14 +51,53 @@ class Beeminder:
     result = self.call_api(url, params, 'POST')
     return result
 
+  def update_datapoint(self, username, goalname, point_id, value, timestamp, daystamp=None, comment=None):
+    url = "%s/users/%s/goals/%s/datapoints/%s.json" % (self.base_url, username, goalname, point_id)
+    params = {
+        'auth_token': self.auth_token,
+        'value': value,
+        'timestamp': timestamp,
+        'daystamp': daystamp,
+        'comment': comment,
+        }
+
+    result = self.call_api(url, params, 'PUT')
+    return result
+
+  def get_datapoint_with_daystamp(self, username, goalname, daystamp):
+    """
+    Fetch the datapoint with the given daystamp. If there are multiple, return the one with highest ID.
+    """
+    assert(type(daystamp) == datetime.date)
+    daystamp_str = '%04d%02d%02d' % (daystamp.year, daystamp.month, daystamp.day) # This is the format of fetched daystamps
+    datapoints = self.get_datapoints(username, goalname, sort='timestamp')
+
+    for datapoint in datapoints[::-1]: # Iterate in reverse order, so we fetch the one with highest ID
+      if 'daystamp' not in datapoint:
+        continue
+      if datapoint['daystamp'] == daystamp_str:
+        return datapoint
+    return None
+
+  def set_day_datapoint(self, username, goalname, value, daystamp, comment=None, requestid=None):
+    """
+    Checks for a datapoint with the given datestamp. If one is found, update it to have given value
+    and comment. Otherwise, create a new datapoint with given daystamp, value, and comment.
+    Returns (whether a new datapoint was created, results of query)
+    """
+    to_replace = self.get_datapoint_with_daystamp(username, goalname, daystamp)
+    if to_replace is None:
+      return (True, self.create_datapoint(username, goalname, value, daystamp=daystamp, comment=comment, requestid=requestid))
+    else:
+      timestamp = datetime.datetime.combine(daystamp, datetime.datetime.min.time())
+      return (False, self.update_datapoint(username, goalname, to_replace['id'], value, timestamp=timestamp, comment=comment))
+
   def call_api(self, url, params, method='GET'):
     pruned_params = {k: v for (k, v) in params.items() if v is not None}
     encoded_params = urlencode(pruned_params).encode('utf-8')
-    if method == 'POST':
-      req = Request(url, encoded_params)
-      response = urlopen(req)
-    else:
-      response = urlopen(url + '?' + encoded_params)
+
+    req = Request(url, encoded_params, method=method)
+    response = urlopen(req)
 
     result = response.read()
     return result
